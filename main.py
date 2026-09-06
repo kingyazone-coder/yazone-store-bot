5772f84c8620484ef3aebc05878cccfa71a7f906
kingyazone-coder <kingyazone@gmail.com>
2026-09-06 10:57:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ STRINGS = {
         'no_orders': "No orders received yet.",
         'no_ads': "No ads posted yet."
     }
-}def sync_db():
+}def sync_db
     save_db({"merchants": merchants_db, "products": products_db, "orders": orders_db, "languages": user_language})
 
 def get_text(user_id, key):
